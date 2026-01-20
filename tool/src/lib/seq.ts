c48e565cb4e40f9955462c2002d7800bf7c32c3f
TsFreddie <whatis@tsdo.in>
2026-01-20 21:10:02 +0800

    [tool] update seq processing

--- tool/src/lib/seq.ts
+++ tool/src/lib/seq.ts
@@ -4,7 +4,59 @@ const addUnique = <T>(arr: T[], item: T) => {
 	if (!arr.includes(item)) arr.push(item);
 };
 
+function isAlphaNum(char: string) {
+	if (char.length !== 1) {
+		return false;
+	}
+	const codePoint = char.codePointAt(0);
+
+	if (!codePoint) {
+		return false;
+	}
+
+	// Check for numeric (0-9)
+	if (codePoint >= 48 && codePoint <= 57) {
+		return true;
+	}
+	// Check for upper alpha (A-Z)
+	if (codePoint >= 65 && codePoint <= 90) {
+		return true;
+	}
+	// Check for lower alpha (a-z)
+	if (codePoint >= 97 && codePoint <= 122) {
+		return true;
+	}
+	return false;
+}
+
 const segmentor = new Intl.Segmenter('zh', { granularity: 'grapheme' });
+const process = (part: string) => {
+	const chars = Array.from(segmentor.segment(part.slice(1))).map((seg) => seg.segment);
+
+	const result = [];
+	let current = '';
+
+	for (const char of chars) {
+		if (part == '⿱亭u丁') {
+			console.log(char);
+		}
+		if (isAlphaNum(char)) {
+			current += char;
+		} else {
+			if (current) {
+				result.push(current);
+				current = '';
+			}
+			current += char;
+		}
+	}
+
+	if (current) {
+		result.push(current);
+	}
+
+	return result;
+};
 
 // IDC sequence database
 export const SEQS = Object.fromEntries(
@@ -16,15 +68,11 @@ export const SEQS = Object.fromEntries(
 
 			for (const part of seqs) {
 				if (part.startsWith('⿰')) {
-					const [left, right] = Array.from(segmentor.segment(part.slice(1))).map(
-						(seg) => seg.segment
-					);
+					const [left, right] = process(part);
 					addUnique(possibleParts, left + '<');
 					addUnique(possibleParts, right + '>');
 				} else if (part.startsWith('⿱')) {
-					const [top, bottom] = Array.from(segmentor.segment(part.slice(1))).map(
-						(seg) => seg.segment
-					);
+					const [top, bottom] = process(part);
 					addUnique(possibleParts, top + '^');
 					addUnique(possibleParts, bottom + 'v');
 				}

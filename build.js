c41f567e9ec745349603151c100b4eaa8aac28dc
TsFreddie <me@tsfreddie.com>
2026-09-16 00:31:21 +0800

    v1.8: bump the pack version

--- build.js
+++ build.js
@@ -3,7 +3,7 @@ import fs from "fs";
 import path from "path";
 
 // Written into every pack's translation header.
-const VERSION = "1.7";
+const VERSION = "1.8";
 
 // Language packs to build. A `po` entry is a vendored catalog from
 // coredevices/pebbleos-translations (see locale/README.md). Older PBF-only
